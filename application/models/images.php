6cb84ad2bdf8698d90f6b1b61391cd5ecdf3b4a5
Manuel Gonzales <mgonzalesaira@my.bcit.ca>
2015-01-18 12:43:20 -0800

    forgot to commit each time I modified a file so this is the whole project, will start committing earlier next time Signed-off-by:Manuel Gonzales <mgonzalesaira@my.bcit.ca>

---
+++ application/models/images.php
@@ -0,0 +1,29 @@
+<?php
+
+class Images extends CI_Model
+{
+
+    function __constructor()
+    {
+        parent::__constructor();
+    }
+
+    // returns an array of all the images
+    function all()
+    {
+        $this->db->order_by('id','desc');
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+    
+     function newest()
+    {
+        $this->db->order_by('id','desc');
+        $this->db->limit(3);
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
+  
+}
+
+?>
